f714a8d5026cfcace9aa6c52cc0624cc28ed2806
nathandodds <nathan@stormcreative.co.uk>
2014-01-02 16:04:29 +0000

    services page update

--- app/views/templates/services/index.php
+++ app/views/templates/services/index.php
@@ -4,16 +4,15 @@
 <div class="container">
 	<article class="services-article">
 		<h2 class="services-sub-title">Standout Services Tailored to your needs</h2>
-		<p>We offer a complete range of services to cater for your school’s every recruitment need. But these aren’t off-the-shelf solutions. Each service is bespoke to the school we supply it to and tailored to meet all financial targets.</p>
-		<h3 class="emerg-supply"><a href="#">Emergency Supply</a></h3>
-		<h3 class="lt-place"><a href="#">Long-term Placements</a></h3>
-		<h3 class="per-place"><a href="#">Permanent Placements</a></h3>
-		<h3 class="sch-sup-staff"><a href="#">School Support Staff</a></h3>
-		<h3 class="sch-maint"><a href="#">School Maintenance</a></h3>
+		<p class="service-article-text">We offer a complete range of services to cater for your school’s every recruitment need. But these aren’t off-the-shelf solutions. Each service is bespoke to the school we supply it to and tailored to meet all financial targets.</p>
+		<a href="#" class="service-btn">Emergency Supply</a>
+		<a href="#" class="service-btn">Long-term Placements</a>
+		<a href="#" class="service-btn">Permanent Placements</a>
+		<a href="#" class="service-btn">School Support Staff</a>
+		<a href="#" class="service-btn">School Maintenance</a>
 		<p class="services-legal-text">All staff supplied to you from AcademyRecruitment.com will have been fully vetted and referenced in accordance with all current legislation guaranteed.</p>  
 	</article>
-	<!--<?php include "assets/includes/article.php"; ?>-->
-	<?php include "assets/includes/aside.php"; ?>
+	<?php include "assets/includes/aside-services.php"; ?>
 </div>
 <div class="footer">
 <?php include "assets/includes/footer.php"; ?>
--- assets/includes/aside-services.php
+++ assets/includes/aside-services.php
@@ -6,18 +6,13 @@
 			<p class="vacancy-text">Looking for staff? We'll help you find the best candidate for your school.</p>
 		</div>
 	</article>
-
-	<a href="<?php echo DIRECTORY; ?>services">
-		<article class="services">
-			<div class="service-img">
-				<h3>Services</h3>
-			</div>
-			<p>Find out about the unique recruitment services we offer schools and other institutions.</p>
-			<p>Find out more &gt;</a></p>
-		</a>
-	</article>
-
-
+	<article class="recommend">
+		<img src="<?php echo DIRECTORY; ?>assets/images/recommend-img.png" class="speech-marks" />
+		<div class="recommend-content"
+			<p class="recommend-text">I would highly recommend this company to anyone who was even thinking about teaching in the UK. They are the most competent, friendly, and professional group of people in the field.</p>
+			<p class="recommend-name">Name Surname, English and Maths teacher</p>
+		</div>
+	</article>	
 	<article class="cv">
 		<img src= "<?php echo DIRECTORY; ?>assets/images/pen-pad.png" class="cv-img"/>
 		<div class="cv-info">
